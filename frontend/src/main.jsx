8cad24a1f8e38d7e446a9823339854d31f1af55d
BV344 <brayan.vizcaino@gmail.com>
2025-10-03 19:18:55 -0700

    Backend - Now stores values that are needed to Create an Account to SQLite DB
    Frontend
    - Now we have a link for new users to create an account. Also, we have a place where users can submit there firstName, lastName, username, & password.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Login                from './pages/Login.jsx'
+import CreateAccount        from './pages/CreateAccount.jsx'
+
 import './styles/main.css'
-import Login from './pages/Login.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Login />
+    <BrowserRouter>
+        <Routes>
+            <Route path="/"                 element={<Login />} />
+            <Route path="/create-account"   element={<CreateAccount />} />
+        </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
